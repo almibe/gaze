69b3154f532d93b6dec51f53fe69f81c3ba2ca8c
berry3 <berry3@indiana.edu>
2021-09-04 18:02:00 -0400

    last commit before 0.1.0 release

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-pub mod raccoons;
-
 use std::ops::Range;
 
 /// The main struct for working with Gaze.
@@ -22,42 +20,6 @@ pub trait Transform<I,O> {
 impl Gaze<'_> {
     fn bite(&mut self, distance: usize) {
         self.offset = self.offset + distance;
-    }
-
-    fn nibble(&mut self, raccoon: &Box<dyn Raccoon>) -> Option<Match> {
-        let start = self.offset;
-        let res = raccoon.taste(self);
-        match res {
-            NibState::Cancel => {
-                self.offset = start;
-                None
-            }
-            NibState::Complete(adjust) => {
-                self.offset = self.offset + adjust;
-                Some(Match {
-                    value: self.input[start..self.offset].to_string(),
-                    range: (start..self.offset + 1),
-                })
-            }
-        }
-    }
-
-    fn nibbles(&mut self, raccoons: &Box<Vec<Box<dyn Raccoon>>>) -> Option<Vec<Match>> {
-        let mut resultList: Vec<Match> = Vec::new();
-        let start = self.offset;
-        for raccoon in raccoons.iter() {
-            let res = self.nibble(raccoon);
-            match res {
-                None => {
-                    self.offset = start;
-                    return None;
-                }
-                Some(value) => {
-                    resultList.push(value);
-                }
-            }
-        }
-        Some(resultList)
     }
 
     fn current_offset(&self) -> usize {
